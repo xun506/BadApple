ec5f7e1821b0a6dcc4f6c6b8f4e565683cc9e330
圣杰 朱 <1134940288@qq.com>
2019-11-22 21:31:25 +0800

    添加项目文件。

---
+++ BadApple/badapple.cpp
@@ -0,0 +1,21 @@
+﻿#include <cstdio>
+#include <windows.h>
+//57行*150位=8550‬一个图
+int main()
+{
+	FILE *fp = fopen("badapple.txt", "r");
+	char buf[8550];
+	while (!feof(fp))
+	{
+		HANDLE hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+		COORD pos = { 0,0 };
+		SetConsoleCursorPosition(hConsoleOutput, pos);
+		Sleep(100);
+		for (int i = 0; i < 58; i++)
+		{
+			fgets(buf, 8550, fp);
+			printf("%s", buf);
+		}
+	}
+	return 0;
+}
